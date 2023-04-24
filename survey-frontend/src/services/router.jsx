d57323d4d4503ae698a4d577055274ecd4455cdd
ojpro <contact@ojpro.me>
2023-04-24 19:45:42 +0100

    added login page

--- survey-frontend/src/services/router.jsx
+++ survey-frontend/src/services/router.jsx
@@ -2,12 +2,17 @@ import {createBrowserRouter} from 'react-router-dom'
 
 // import pages
 import App from '../App.jsx'
+import Login from "../pages/Login.jsx";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App/>
   },
+  {
+    path: '/login',
+    element: <Login/>
+  }
 ]);
 
 export default router;
