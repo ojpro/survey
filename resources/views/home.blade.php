eca51034604cdcc96c026e7dabe2ea4ab6433de5
ojpro <contact@ojpro.me>
2023-04-24 18:54:22 +0100

    fresh laravel app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Survey</title>
+
+    </head>
+    <body class="antialiased">
+        <h1>Hey World!</h1>
+    </body>
+</html>
