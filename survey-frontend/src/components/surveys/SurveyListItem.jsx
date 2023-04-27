2434da7a41e83d4cc81fb87cdbaff83438a1170b
ojpro <contact@ojpro.me>
2023-04-27 23:35:05 +0100

    created survey item component

---
+++ survey-frontend/src/components/surveys/SurveyListItem.jsx
@@ -0,0 +1,28 @@
+import CustomButton from "../core/CustomButton.jsx";
+import {CiEdit} from "react-icons/ci";
+import {FiExternalLink, FiTrash} from "react-icons/fi";
+
+export default function SurveyListItem({survey,handleClick=()=>{}}) {
+  return (
+    <>
+      <div className='shadow p-4'>
+        <img src={survey.image_url} alt={survey.title} className='w-full max-h-fit object-cover'/>
+        <h3>{survey.title}</h3>
+        <div dangerouslySetInnerHTML={{__html: survey.description}} className='overflow-hidden'></div>
+        <div className='flex flex-row justify-between gap-2 items-center'>
+          <CustomButton>
+            <CiEdit className='w-5 h-5'/> Edit
+          </CustomButton>
+          <div className='flex flex-row flex-nowrap justify-around items-center gap-2'>
+            <CustomButton link circle color={'gray'}>
+              <FiExternalLink className='w-5 h-5'/>
+            </CustomButton>
+            <CustomButton link circle color={'red'}>
+              <FiTrash className='w-5 h-5'/>
+            </CustomButton>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- survey-frontend/src/views/Surveys.jsx
+++ survey-frontend/src/views/Surveys.jsx
@@ -1,14 +1,19 @@
 import DashboardPage from "../components/DashboardPage.jsx";
 import {userStateContext} from "../contexts/ContextProvider.jsx";
+import SurveyListItem from "../components/surveys/SurveyListItem.jsx";
 
-export default function Surveys(){
+export default function Surveys() {
   const {surveys} = userStateContext();
-  console.log(surveys)
+
   return (
-   <>
-     <DashboardPage title='Surveys'>
-      List of surveys
-     </DashboardPage>
-   </>
+    <>
+      <DashboardPage title='Surveys'>
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 justify-around items-center'>
+          {surveys.map((survey) => (
+            <SurveyListItem survey={survey} key={survey.id}/>
+          ))}
+        </div>
+      </DashboardPage>
+    </>
   )
 }
