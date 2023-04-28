73ec2e9429ef81acfdb5920b3a34a32ae85d567a
ojpro <contact@ojpro.me>
2023-04-28 14:26:59 +0100

    added create survey view

---
+++ survey-frontend/src/views/CreateSurvey.jsx
@@ -0,0 +1,11 @@
+import DashboardPage from "../components/DashboardPage.jsx";
+
+export default function CreateSurvey() {
+  return (
+    <>
+      <DashboardPage title='Create new Survey'>
+        Create new Survey
+      </DashboardPage>
+    </>
+  );
+}
