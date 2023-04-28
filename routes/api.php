f9bcf622f5067483972031886919568d86da1f56
ojpro <contact@ojpro.me>
2023-04-28 22:12:45 +0100

    added auth method to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,7 @@
 <?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Routing\Route;
+
+Route::get('/signup', [AuthController::class, 'signup']);
+Route::get('/login', [AuthController::class, 'login']);
