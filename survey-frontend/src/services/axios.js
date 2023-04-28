ae2147ea0b08d905406ba5ee0825535fa0990dd6
ojpro <contact@ojpro.me>
2023-04-28 22:11:44 +0100

    created axios client service

---
+++ survey-frontend/src/services/axios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import router from "./router.jsx";
+
+const Axios = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+})
+
+// set requests' header authorization token
+Axios.interceptors.request.use(config => {
+  const token = "tempToken"; // TODO
+  config.headers.Authorization = `Bearer ${token}`;
+})
+
+// Handle Unauthorized requests
+Axios.interceptors.response.use(response => {
+  return response;
+}, error => {
+  // if the response not authorized
+  if (error.response && error.response.status === 401) {
+    // redirect to the login page
+    router.navigate('/login');
+    return error;
+  }
+  throw error;
+})
+export default Axios
