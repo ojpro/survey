24f3bd9d13a456fef5dcc46b2350afa2799065d9
ojpro <contact@ojpro.me>
2023-04-29 20:59:04 +0100

    added survey's api resource

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use \Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('survey',\App\Http\Controllers\SurveyController::class);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
