22a0a1abdb65370e557763dac9c7a929cb8ea093
ojpro <contact@ojpro.me>
2023-04-29 20:56:48 +0100

    added survey model

---
+++ app/Models/Survey.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Survey extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'title', 'image', 'slug', 'user_id', 'description', 'expire_date', 'status'
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+}
