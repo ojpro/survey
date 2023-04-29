c3661d7368754abc64674e6a4293c6a1356b89a0
ojpro <contact@ojpro.me>
2023-04-29 21:00:09 +0100

    consume survey's api and manage handle image upload

--- survey-frontend/src/views/CreateSurvey.jsx
+++ survey-frontend/src/views/CreateSurvey.jsx
@@ -3,11 +3,13 @@ import {useState} from "react";
 import {FiSave} from "react-icons/fi";
 import {HiOutlinePhoto} from "react-icons/hi2";
 import CustomButton from "../components/core/CustomButton.jsx";
+import Axios from "../services/axios.js";
+import {useNavigate} from "react-router-dom";
 
 export default function CreateSurvey() {
+  const navigate = useNavigate()
   const [survey, setSurvey] = useState({
     title: "",
-    slug: "",
     status: false,
     description: "",
     image: null,
@@ -15,16 +17,49 @@ export default function CreateSurvey() {
     expire_date: "",
     questions: []
   })
+  const [errors, setErrors] = useState({})
 
   // handle form submit
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log('submit')
+
+    const payload = {...survey};
+
+    // set the image_url to be the data URI (base64 form)
+    if (payload.image) {
+      payload.image = payload.image_url;
+    }
+
+    delete payload.image_url;
+
+    Axios.post('/survey', payload)
+      .then(response => {
+        console.log(response)
+        navigate('/surveys');
+      })
+      .catch(({response}) => {
+        setErrors(response.data.errors)
+        console.log(errors)
+      })
   }
 
   // handle image upload
-  const handleImageUpload = () => {
-    console.log('image update')
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader()
+
+    reader.onload = (result) => {
+      setSurvey({
+        ...survey,
+        image: file,
+        image_url: reader.result
+      })
+
+      event.target.value = '';
+    }
+
+    reader.readAsDataURL(file);
   }
 
   return (
@@ -56,7 +91,8 @@ export default function CreateSurvey() {
                 <button
                   type="button"
                   className="relative ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                  <input type="file" id='image_field' className='absolute inset-0 opacity-0' onChange={handleImageUpload}/>
+                  <input type="file" id='image_field' className='absolute inset-0 opacity-0'
+                         onChange={handleImageUpload}/>
                   Change
                 </button>
               </div>
@@ -70,6 +106,11 @@ export default function CreateSurvey() {
                 <input type="text" name='title' id='title' value={survey.title} placeholder='Survey Title'
                        className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-50 focus:ring-2 focus:ring-blue-500 sm:text-sm'
                        onChange={(event) => setSurvey({...survey, title: event.target.value})}/>
+                {errors.title && (
+                  <span className='text-sm text-red-500 p-1'>
+                      {errors.title}
+                  </span>
+                )}
               </div>
               {/* // Survey Title */}
 
@@ -93,13 +134,20 @@ export default function CreateSurvey() {
                 <input type="date" name='expire_date' id='expire_date' value={survey.expire_date}
                        className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-50 focus:ring-2 focus:ring-blue-500 sm:text-sm'
                        onChange={(event) => setSurvey({...survey, expire_date: event.target.value})}/>
+                {errors.expire_date && (
+                  <span className='text-sm text-red-500 p-1'>
+                      {errors.expire_date}
+                  </span>
+                )}
               </div>
               {/* // Survey Expire Date */}
 
               {/* Survey Status */}
               <div className="flex items-start">
                 <div className="flex h-5 items-center">
-                  <input type="checkbox" id="status" name="status" checked={survey.status} className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" onChange={(event)=> setSurvey({...survey,status: event.target.checked}) } />
+                  <input type="checkbox" id="status" name="status" checked={survey.status}
+                         className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                         onChange={(event) => setSurvey({...survey, status: event.target.checked})}/>
                 </div>
                 <div className='ml-3 text-sm'>
                   <label htmlFor="" className="font-medium text-gray-700">
