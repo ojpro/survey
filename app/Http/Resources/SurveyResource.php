51366e1dc260a1161c5bdc18f54c3b0b55470a86
ojpro <contact@ojpro.me>
2023-04-30 15:42:36 +0100

    edit Survey Resource class

--- app/Http/Resources/SurveyResource.php
+++ app/Http/Resources/SurveyResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 
 class SurveyResource extends JsonResource
 {
@@ -15,9 +16,14 @@ class SurveyResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'=>$this->id,
-            'title'=>$this->title,
-            'slug'=>$this->slug,
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'slug' => $this->slug,
+            'image_url' => $this->image ? URL::to($this->image) : null,
+            'status' => !!$this->status,
+            'expire_date' => $this->expire_date,
+            'created_at' => $this->created_at->diffForHumans()
         ];
     }
 }
