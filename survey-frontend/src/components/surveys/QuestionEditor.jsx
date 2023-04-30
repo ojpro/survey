4df240f84f19a44ff8e719a244d3c8bf5df8416b
ojpro <contact@ojpro.me>
2023-04-30 19:12:26 +0100

    bug fix

--- survey-frontend/src/components/surveys/QuestionEditor.jsx
+++ survey-frontend/src/components/surveys/QuestionEditor.jsx
@@ -60,7 +60,7 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
           <label htmlFor="description" className='block text-sm font-medium text-gray-700'>
             Description
           </label>
-          <textarea name="description" id="description" value={model.description}
+          <textarea name="description" id="description" value={model.description || ''}
                     onChange={(event) => setModel({...model, description: event.target.value})}
                     className='mt-1 block w-full rounded-md border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'></textarea>
         </div>
