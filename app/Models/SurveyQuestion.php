06265465483ebe2ed379d3c8d95a754a4960dca2
ojpro <contact@ojpro.me>
2023-04-30 13:56:06 +0100

    set fill able field of the survey questions table

---
+++ app/Models/SurveyQuestion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SurveyQuestion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type', 'question', 'description', 'data', 'survey_id'
+    ];
+}
