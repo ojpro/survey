37ff514c523376911f504a9b84d5cff7f426f284
ojpro <contact@ojpro.me>
2023-05-01 12:22:15 +0100

    Implemented question's options functionality

--- survey-frontend/src/components/surveys/QuestionEditor.jsx
+++ survey-frontend/src/components/surveys/QuestionEditor.jsx
@@ -1,6 +1,9 @@
 import {useEffect, useState} from "react";
 import {useStateContext} from "../../contexts/ContextProvider.jsx";
-import { BiTrash} from "react-icons/bi";
+import {BiTrash} from "react-icons/bi";
+import {v4 as uuid} from 'uuid';
+import CustomButton from "../core/CustomButton.jsx";
+import {FiPlusCircle, FiTrash} from "react-icons/fi";
 
 export default function QuestionEditor({index = 0, question, deleteQuestion, questionChange}) {
   const [model, setModel] = useState({...question});
@@ -11,7 +14,52 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
     questionChange(model)
   }, [model])
 
-  // handy function to uppercase the first letter
+  const onTypeChange = (event) => {
+    // TODO: should empty option if the selected type does not need them
+    // create a variable to store question
+    const question = {...model, type: event.target.value}
+    // add option if needed
+    if (shouldHaveOptions(question.type)) {
+      question.data = {
+        options: [
+          {
+            uuid: uuid(),
+            text: ''
+          }
+        ]
+      }
+    }
+    // update question model
+    setModel(question)
+  }
+
+  const addOption = () => {
+    model.data.options.push({uuid: uuid(), text: ''})
+    setModel({...model})
+  }
+
+  const onOptionChange = (event, optionId) => {
+    const value = event.target.value
+    model.data.options.map((option, index) => {
+      if (option.uuid === optionId) {
+        model.data.options[index].text = value
+      }
+      return option
+    })
+    setModel({...model})
+  }
+
+  const deleteOption = (uuid) => {
+    model.data.options = model.data.options.filter((option) => option.uuid !== uuid)
+    setModel({...model})
+  }
+  // check if the question should display options
+  const shouldHaveOptions = (type = null) => {
+    let questionType = type || model.type;
+    return ['select', 'checkbox', 'radio'].includes(questionType)
+  };
+
+  // function to uppercase the first letter
   const upperCaseFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1)
   return (
     <>
@@ -46,7 +94,7 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
               Question type
             </label>
             <select name="question_type" id="question_type" value={model.type}
-                    onChange={(event) => setModel({...model, type: event.target.value})}
+                    onChange={onTypeChange}
                     className='mt-1 block w-full rounded-md border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'>
               {questionTypes.map((type) => (
                 <option value={type} key={type}>{upperCaseFirst(type)}</option>
@@ -55,6 +103,46 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
           </div>
           {/* // Question Type */}
         </div>
+
+        {/* Question Options */}
+        {shouldHaveOptions() && (
+          <div className='my-4 p-2'>
+            <div className='flex flex-row justify-between items-center'>
+              <h4 className='text text-gray-700'>Options</h4>
+              <CustomButton handleClick={addOption}>
+                <FiPlusCircle className="w-5 h-5"/> New Option
+              </CustomButton>
+            </div>
+            {(!model.data.options || model.data.options.length === 0) && (
+              <div className='text-gray-800 text-center my-4'>
+                Question doesn't have any option.
+              </div>
+            )}
+
+            {model.data.options && model.data.options.length > 0 && (
+              <div>
+                {model.data.options.map((option, index) => (
+                  <div key={index}>
+                    <div className='flex flex-row justify-between gap-4 items-center'>
+                      <label htmlFor={`option-${index}`} className='block text-sm font-medium text-gray-700'>
+                        {index + 1}.
+                      </label>
+                      <input type="text" id={`option-${index}`} name='option' value={option.text || ' '}
+                             placeholder='Option value' onChange={event => onOptionChange(event, option.uuid)}
+                             className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'/>
+
+                      <CustomButton link circle color={'red'} handleClick={() => deleteOption(option.uuid)}>
+                        <FiTrash className='w-5 h-5'/>
+                      </CustomButton>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        )}
+        {/* // Question Options */}
+
         {/* Question Description */}
         <div>
           <label htmlFor="description" className='block text-sm font-medium text-gray-700'>
@@ -66,6 +154,7 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
         </div>
         {/* // Question Description */}
       </div>
+      <hr/>
     </>
   );
 }
