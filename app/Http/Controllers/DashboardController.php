4f1673ae9d534487bdb5e158da4d0acfbcae1c5a
ojpro <contact@ojpro.me>
2023-05-05 00:54:14 +0100

    added trending surveys

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,13 @@ use App\Models\Survey;
 use App\Models\SurveyAnswer;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
+        // TODO : refactor all this
 
         $user = $request->user();
         $startOfMonth = Carbon::now()->startOfMonth()->toDateString();
@@ -27,7 +29,7 @@ class DashboardController extends Controller
             ->join('surveys', 'survey_answers.survey_id', '=', 'surveys.id')
             ->where('surveys.user_id', $user->id)
             ->whereBetween('start_date', [$startOfMonth, $endOfMonth])
-            ->count();
+            ->get();
 
         // total answers in the last month
         $total_answers_last_month = SurveyAnswer::query()
@@ -38,9 +40,8 @@ class DashboardController extends Controller
 
         // percentage of
         if ($total_answers_last_month !== 0) {
-            $percentageChange = (($total_answers - $total_answers_last_month) / $total_answers_last_month) * 100;
+            $percentageChange = (($total_answers->count() - $total_answers_last_month) / $total_answers_last_month) * 100;
         }
-
 
         // latest answers in
         $latest_answer_date = SurveyAnswer::join('surveys', 'survey_answers.survey_id', '=', 'surveys.id')
@@ -59,21 +60,59 @@ class DashboardController extends Controller
             ->whereNull('survey_answers.end_date')
             ->count();
 
+        $topSurveys = Survey::select('surveys.*', DB::raw('COUNT(survey_answers.id) as total_answers'))
+            ->where('surveys.user_id', $user->id)
+            ->leftJoin('survey_answers', 'surveys.id', '=', 'survey_answers.survey_id')
+            ->groupBy('surveys.id')
+            ->orderByDesc('total_answers')
+            ->limit(10)
+            ->get();
+
+
         // percentage of completed answers
         if ($total_surveys > 0) {
-            $completed_percentage = ($total_completed_answers / $total_answers) * 100;
+            $completed_percentage = ($total_completed_answers / $total_answers->count()) * 100;
         } else {
             $completed_percentage = 0;
         }
 
+        // Initialize empty arrays for the x-axis and y-axis data
+        $labels = [];
+        $data = [];
+
+        // Loop through each SurveyAnswer and extract the relevant data
+        foreach ($total_answers as $answer) {
+            // Get the date of the SurveyAnswer and format it as desired
+            $date = date('Y-m-d', strtotime($answer->start_date));
+
+            // If this date has not been encountered before, add it to the labels array
+            if (!in_array($date, $labels)) {
+                $labels[] = $date;
+            }
+
+            // Increment the count for this date in the data array
+            if (isset($data[$date])) {
+                $data[$date]++;
+            } else {
+                $data[$date] = 1;
+            }
+        }
+
+        // Convert the labels and data arrays to JSON format
+        $labels_json = $labels;
+        $data_json = array_values($data);
+
         return [
             'total_surveys' => $total_surveys,
-            'total_answers' => $total_answers,
+            'total_answers' => $total_answers->count(),
+            'answers_labels' => $labels_json,
+            'answers_data' => $data_json,
             'latest_answer_date' => \Carbon\Carbon::parse($latest_answer_date['end_date'])->diffForHumans(),
-            'total_completed_surveys' => $total_completed_answers,
+            'total_completed_answers' => $total_completed_answers,
             'completed_percentage' => round($completed_percentage),
             'total_remaining_answers' => $total_remaining_answers,
-            'answers_compared_to_last_month_percentage' => $percentageChange ?? 0
+            'answers_compared_to_last_month_percentage' => $percentageChange ?? 0,
+            'top_surveys' => $topSurveys
         ];
     }
 }
