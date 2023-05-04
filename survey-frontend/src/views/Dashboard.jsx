874eda31ba3db715c487f04cffa81928ac941ae5
ojpro <contact@ojpro.me>
2023-05-04 02:42:13 +0100

    use some beautiful stat component from daisyUI

--- survey-frontend/src/views/Dashboard.jsx
+++ survey-frontend/src/views/Dashboard.jsx
@@ -1,6 +1,9 @@
 import DashboardPage from "../components/DashboardPage.jsx";
 import {useEffect, useState} from "react";
 import Axios from "../services/axios.js";
+import {BsCalendarCheck, BsJournalCheck} from "react-icons/bs";
+import {HiOutlineClock} from "react-icons/hi2";
+import {SiAnswer} from "react-icons/all.js";
 
 export default function Dashboard() {
   const [statistics, setStatistics] = useState({});
@@ -21,22 +24,43 @@ export default function Dashboard() {
         )}
 
         {statistics && (
-          <div className='m-4 flex flex-row flex-wrap gap-5 justify-start items-center'>
-            <div className='shadow-md p-4 rounded-md w-64'>
-              <span className='text-gray-600 text-right text-lg'>Total Surveys</span>
-              <br/>
-              <span className='mt-2 block text-gray-800 text-lg font-semibold'>{statistics.total_surveys}</span>
+          <div className="stats shadow flex flex-row flex-wrap">
+
+            <div className="stat flex-1">
+              <div className="stat-figure text-primary">
+                <BsJournalCheck className='w-6 h-6'/>
+              </div>
+              <div className="stat-title">Total Surveys</div>
+              <div className="stat-value text-primary p-1">{statistics.total_surveys}</div>
             </div>
-            <div className='shadow-md p-4 rounded-md w-64'>
-              <span className='text-gray-600 text-right text-lg'>Total Answers</span>
-              <br/>
-              <span className='mt-2 block text-gray-800 text-lg font-semibold'>{statistics.total_answers}</span>
+
+            <div className="stat flex-1">
+              <div className="stat-figure text-secondary">
+                <SiAnswer className='w-6 h-6'/>
+              </div>
+              <div className="stat-title">Total Answers</div>
+              <div className="stat-value text-secondary">{statistics.total_answers}</div>
+              <div className="stat-desc">{statistics.answers_compared_to_last_month_percentage}% more than last month
+              </div>
             </div>
-            <div className='shadow-md p-4 rounded-md w-64'>
-              <span className='text-gray-600 text-right text-lg'>Total Surveys</span>
-              <br/>
-              <span className='mt-2 block text-gray-800 text-lg font-semibold'>{statistics.latest_answer_date}</span>
+
+            <div className="stat flex-1">
+              <div className="stat-figure text-accent">
+                <HiOutlineClock className='w-7 h-7'/>
+              </div>
+              <div className="stat-title">Latest Answer in</div>
+              <div className="stat-value text-accent text-3xl">{statistics.latest_answer_date}</div>
             </div>
+
+            <div className="stat flex-1">
+              <div className="stat-figure text-orange-500">
+                <BsCalendarCheck className='w-6 h-6'/>
+              </div>
+              <div className="stat-value">{statistics.completed_percentage}%</div>
+              <div className="stat-title">Answers completed</div>
+              <div className="stat-desc text-orange-500">{statistics.total_remaining_answers} answers remaining</div>
+            </div>
+
           </div>
         )}
       </DashboardPage>
