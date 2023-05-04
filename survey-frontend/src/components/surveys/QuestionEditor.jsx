a16bf6916444c2cf1e366852be8f4092f752fd12
ojpro <contact@ojpro.me>
2023-05-04 03:05:24 +0100

    little edits on question editor

--- survey-frontend/src/components/surveys/QuestionEditor.jsx
+++ survey-frontend/src/components/surveys/QuestionEditor.jsx
@@ -84,18 +84,19 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
             </label>
             <input type="text" id='question' name='question' value={model.question}
                    onChange={(event) => setModel({...model, question: event.target.value})}
-                   className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'/>
+                   className='block input border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm w-full'/>
           </div>
           {/* // Question Text */}
 
           {/* Question Type */}
+
           <div>
             <label htmlFor="question_type" className='block text-sm font-medium text-gray-700 w-40'>
               Question type
             </label>
             <select name="question_type" id="question_type" value={model.type}
                     onChange={onTypeChange}
-                    className='mt-1 block w-full rounded-md border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'>
+                    className='select border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'>
               {questionTypes.map((type) => (
                 <option value={type} key={type}>{upperCaseFirst(type)}</option>
               ))}
@@ -112,7 +113,7 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
           </label>
           <textarea name="description" id="description" value={model.description || ''}
                     onChange={(event) => setModel({...model, description: event.target.value})}
-                    className='mt-1 block w-full rounded-md border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'></textarea>
+                    className='mt-1 block border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm w-full'></textarea>
         </div>
         {/* // Question Description */}
 
@@ -132,7 +133,7 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
             )}
 
             {model.data.options && model.data.options.length > 0 && (
-              <div>
+              <div className='flex flex-col gap-2'>
                 {model.data.options.map((option, index) => (
                   <div key={index}>
                     <div className='flex flex-row justify-between gap-4 items-center'>
@@ -141,7 +142,7 @@ export default function QuestionEditor({index = 0, question, deleteQuestion, que
                       </label>
                       <input type="text" id={`option-${index}`} name='option' value={option.text || ' '}
                              placeholder='Option value' onChange={event => onOptionChange(event, option.uuid)}
-                             className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm'/>
+                             className='input block border-gray-300 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm w-full'/>
 
                       <CustomButton link circle color={'red'} handleClick={() => deleteOption(option.uuid)}>
                         <FiTrash className='w-5 h-5'/>
