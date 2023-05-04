cd50a6efeb6219a55067fd0bc84bfd2473bcbd86
ojpro <contact@ojpro.me>
2023-05-04 01:15:20 +0100

    minor edits on dashboard page component

--- survey-frontend/src/components/DashboardPage.jsx
+++ survey-frontend/src/components/DashboardPage.jsx
@@ -1,7 +1,7 @@
 export default function DashboardPage({title, buttons = '', children}) {
   return (
     <>
-      <header className="bg-white shadow">
+      <header>
         <div
           className="flex flex-row flex-wrap justify-between items-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">{title}</h1>
