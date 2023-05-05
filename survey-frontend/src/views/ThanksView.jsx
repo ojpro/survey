81f35f2d3782f0bca94147a37392da3fe79a6465
ojpro <contact@ojpro.me>
2023-05-05 15:39:41 +0100

    added thanks page

---
+++ survey-frontend/src/views/ThanksView.jsx
@@ -0,0 +1,13 @@
+export const ThanksView = () => {
+  return (
+    <div className='md:w-1/2 mt-24 mx-auto p-4'>
+      <div className="mockup-window border bg-info">
+        <div className="flex justify-center px-4 py-16 bg-base-200">
+          <span className='text-xl font-medium text-gray-700'>
+            🎉 Thank you for your time, we appreciate your honest answers.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
