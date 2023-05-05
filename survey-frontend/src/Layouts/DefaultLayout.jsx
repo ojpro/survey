38d8938dc80be305ded4046a1d2a3a8177fb40e3
ojpro <contact@ojpro.me>
2023-05-05 15:14:25 +0100

    minor improvement on the navbar colors

--- survey-frontend/src/Layouts/DefaultLayout.jsx
+++ survey-frontend/src/Layouts/DefaultLayout.jsx
@@ -5,10 +5,7 @@ import {FiUser} from "react-icons/fi";
 import Axios from "../services/axios.js";
 import Toast from "../components/core/Toast.jsx";
 
-const navigation = [
-  {name: 'Dashboard', to: '/'},
-  {name: 'Surveys', to: '/surveys'},
-]
+const navigation = [{name: 'Dashboard', to: '/'}, {name: 'Surveys', to: '/surveys'},]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -46,87 +43,80 @@ export default function DefaultLayout() {
       })
   }, [])
 
-  return (
-    <>
-      <Toast/>
+  return (<>
+    <Toast/>
 
-      <div className="min-h-full container mx-auto p-1">
+    <div className="min-h-full container mx-auto p-1">
 
-        {/* Navbar */}
-        <div className="navbar bg-blue-400 m-3 rounded-full px-8">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost focus:bg-blue-500 lg:hidden text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16"/>
-                </svg>
-              </label>
-              <ul tabIndex={0}
-                  className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      {/* Navbar */}
+      <div className="navbar bg-info text-info-content m-3 rounded-full px-8">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                   stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16"/>
+              </svg>
+            </label>
+            <ul tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
 
-                {navigation.map((item, index) => (
-                  <li key={index}>
-                    <NavLink
-                      key={item.name}
-                      to={item.to}
-                      className={({isActive}) => classNames('focus:bg-blue-500 focus:text-white')}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </NavLink>
-                  </li>
-                ))}
+              {navigation.map((item, index) => (<li key={index}>
+                <NavLink
+                  key={item.name}
+                  to={item.to}
+                  className={({isActive}) => classNames('btn btn-ghost')}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </NavLink>
+              </li>))}
 
-              </ul>
-            </div>
-            <NavLink to='/' className="btn btn-ghost normal-case text-xl text-white">Surveys</NavLink>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              {navigation.map((item, index) => (
-                <li key={index}>
-                  <NavLink
-                    key={item.name}
-                    to={item.to}
-                    className={({isActive}) => classNames('focus:bg-blue-500 text-white')}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </NavLink>
-                </li>
-              ))}
             </ul>
           </div>
-          <div className="navbar-end">
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <FiUser className="w-6 h-6 text-white"/>
-              </label>
-              <ul tabIndex={0}
-                  className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li><a>Settings</a></li>
-                <li>
-                  <a href=""
-                     onClick={(event) => logout(event)}>Logout</a>
-                </li>
-              </ul>
-            </div>
+          <NavLink to='/' className="btn btn-ghost bg-info normal-case text-xl text-white">Surveys</NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-white">
+            {navigation.map((item, index) => (<li key={index}>
+              <NavLink
+                key={item.name}
+                to={item.to}
+                className={({isActive}) => classNames('btn btn-ghost')}
+                aria-current={item.current ? 'page' : undefined}
+              >
+                {item.name}
+              </NavLink>
+            </li>))}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <FiUser className="w-6 h-6 text-white"/>
+            </label>
+            <ul tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow-md bg-base-100 rounded-box w-52">
+              <li>
+                <a className="btn btn-ghost">
+                  Profile
+                </a>
+              </li>
+              <li><a className='btn btn-ghost'>Settings</a></li>
+              <li>
+                <a href="" className='btn btn-ghost'
+                   onClick={(event) => logout(event)}>Logout</a>
+              </li>
+            </ul>
           </div>
         </div>
-        {/* //Navbar */}
-
-        {/* Content */}
-        <main>
-          <Outlet/>
-        </main>
       </div>
-    </>
-  )
+      {/* //Navbar */}
+
+      {/* Content */}
+      <main>
+        <Outlet/>
+      </main>
+    </div>
+  </>)
 }
